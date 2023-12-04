8df01156a573be36d810447457a344bed35a93d8
KevinAHL <139507581+KevinAHL@users.noreply.github.com>
2023-12-04 14:00:36 -0400

    Add files via upload

---
+++ DeliciasNicole-main/static/node.js
@@ -0,0 +1,11 @@
+//  const fs = require("fs");
+
+//  fs.writeFile('message.txt', "Hola esta es una prueba", (err)=>{
+//     if (err) throw err;
+//     console.log('Se guardó bien')
+//  });
+
+//  fs.readFile('message.txt', 'utf-8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+//   }); 
